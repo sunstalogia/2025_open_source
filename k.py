69843e596632bc1cd3380cb593cafe966d4cde7b
sunstalogia <sunstalogia@gmail.com>
2025-04-02 15:37:06 +0900

    v02 kiosk

--- k.py
+++ k.py
@@ -1,4 +1,15 @@
-coffee_price = 2400
-cups = int(input("몇 잔 주문 하시나요? "))
-#print(cups * coffee_price)
-print(f"커피 {cups}잔을 주문하셨습니다\n총 금액은 {cups*coffee_price}원 입니다")
+# 메뉴 : 1) 아이스 아메리카노  2) 카페 라떼
+# 가격 : 1) 2000원   2) 2500원
+
+menu01 = 0
+menu02 = 0
+amount = int(input("몇 잔? "))
+
+for _ in range(amount):  # 입력 횟수 만큼 반복
+    menu = input("1) 아이스 아메리카노  2) 카페 라떼 : ")
+    if menu == "1":
+        menu01 = menu01 + 1
+    else:
+        menu02 = menu02 + 1
+
+print(f"총 금액은 {(menu01 * 2000) + (menu02 * 2500)}원 입니다.")
