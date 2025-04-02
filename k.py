282edf5780b6cd0d1e2e10afdc8f473b320e0ae7
sunstalogia <sunstalogia@gmail.com>
2025-04-02 15:47:08 +0900

    v06 kiosk

--- k.py
+++ k.py
@@ -1,4 +1,4 @@
-# 총 계산 금액 확장 시 불편한 점 개선
+# 주문 할 때마다 주문 상황을 디스플레이
 
 menus = [["아이스 아메리카노", 0], ["카페 라떼", 0], ["유자차", 0], ["자바칩 프라푸치노", 0]]  # [[메뉴, 수량], ...]
 prices = [2000, 2500, 2400, 7000]
@@ -13,12 +13,16 @@ for _ in range(amount):
     menu = input(f"{menu_lists}: ")
     if menu == "1":
         menus[0][1] = menus[0][1] + 1
+        print(f"{menus[0][0]} {menus[0][1]}개 주문...")
     elif menu == "2":
         menus[1][1] = menus[1][1] + 1
+        print(f"{menus[1][0]} {menus[1][1]}개 주문...")
     elif menu == "3":
         menus[2][1] = menus[2][1] + 1
+        print(f"{menus[2][0]} {menus[2][1]}개 주문...")
     elif menu == "4":
         menus[3][1] = menus[3][1] + 1
+        print(f"{menus[3][0]} {menus[3][1]}개 주문...")
     else:
         print("잘못된 주문입니다")
 
