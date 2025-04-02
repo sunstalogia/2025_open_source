7f07f97bc8f0be10701a289c1bcf598fb31d2f74
sunstalogia <sunstalogia@gmail.com>
2025-04-02 15:43:38 +0900

    v03 kiosk

--- k.py
+++ k.py
@@ -1,15 +1,17 @@
-# 메뉴 : 1) 아이스 아메리카노  2) 카페 라떼
-# 가격 : 1) 2000원   2) 2500원
+# 리스트 -> 메뉴 리스트
 
-menu01 = 0
-menu02 = 0
+menus = [["아이스 아메리카노", 0], ["카페 라떼", 0], ["유자차", 0]]  # [[메뉴, 수량], ...]
+#prices = [2000, 2500, 2400]
+
 amount = int(input("몇 잔? "))
 
-for _ in range(amount):  # 입력 횟수 만큼 반복
-    menu = input("1) 아이스 아메리카노  2) 카페 라떼 : ")
+for _ in range(amount):
+    menu = input(f"1) {menus[0][0]}   2) {menus[1][0]}   3) {menus[2][0]} : ")
     if menu == "1":
-        menu01 = menu01 + 1
+        menus[0][1] = menus[0][1] + 1
+    elif menu == "2":
+        menus[1][1] = menus[1][1] + 1
     else:
-        menu02 = menu02 + 1
+        menus[2][1] = menus[2][1] + 1
 
-print(f"총 금액은 {(menu01 * 2000) + (menu02 * 2500)}원 입니다.")
+print(f"총 금액은 {(menus[0][1] * 2000) + (menus[1][1] * 2500) + (menus[2][1] * 2400)}원 입니다.")
