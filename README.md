5ca91aa7d784627d21fdcc4f261a514b28780ba9
sunstalogia <sunstalogia@gmail.com>
2025-04-02 15:35:39 +0900

    v0.1 kiosk & README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Cafe Kiosk Project Repository
---
+++ k.py
@@ -0,0 +1,4 @@
+coffee_price = 2400
+cups = int(input("몇 잔 주문 하시나요? "))
+#print(cups * coffee_price)
+print(f"커피 {cups}잔을 주문하셨습니다\n총 금액은 {cups*coffee_price}원 입니다")
