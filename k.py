162cc5782e7f8a0e1a189edc309f5b8475596942
sunstalogia <sunstalogia@gmail.com>
2025-04-02 15:44:48 +0900

    v04 kiosk

--- k.py
+++ k.py
@@ -1,12 +1,17 @@
-# 리스트 -> 메뉴 리스트
+# 메뉴 확장 시 불편한 점 개선
 
 menus = [["아이스 아메리카노", 0], ["카페 라떼", 0], ["유자차", 0]]  # [[메뉴, 수량], ...]
 #prices = [2000, 2500, 2400]
 
+#print(len(menus))
 amount = int(input("몇 잔? "))
 
+menu_lists = ""
+for i in range(len(menus)):
+    menu_lists = menu_lists + f"{i+1}) {menus[i][0]} "
+
 for _ in range(amount):
-    menu = input(f"1) {menus[0][0]}   2) {menus[1][0]}   3) {menus[2][0]} : ")
+    menu = input(f"{menu_lists}: ")
     if menu == "1":
         menus[0][1] = menus[0][1] + 1
     elif menu == "2":
