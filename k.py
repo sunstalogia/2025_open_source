cbeda97bf13ad8095c7a51f4b64b8c69476d21fc
sunstalogia <sunstalogia@gmail.com>
2025-04-02 15:46:17 +0900

    v05 kiosk

--- k.py
+++ k.py
@@ -1,9 +1,8 @@
-# 메뉴 확장 시 불편한 점 개선
+# 총 계산 금액 확장 시 불편한 점 개선
 
-menus = [["아이스 아메리카노", 0], ["카페 라떼", 0], ["유자차", 0]]  # [[메뉴, 수량], ...]
-#prices = [2000, 2500, 2400]
+menus = [["아이스 아메리카노", 0], ["카페 라떼", 0], ["유자차", 0], ["자바칩 프라푸치노", 0]]  # [[메뉴, 수량], ...]
+prices = [2000, 2500, 2400, 7000]
 
-#print(len(menus))
 amount = int(input("몇 잔? "))
 
 menu_lists = ""
@@ -16,7 +15,18 @@ for _ in range(amount):
         menus[0][1] = menus[0][1] + 1
     elif menu == "2":
         menus[1][1] = menus[1][1] + 1
-    else:
+    elif menu == "3":
         menus[2][1] = menus[2][1] + 1
+    elif menu == "4":
+        menus[3][1] = menus[3][1] + 1
+    else:
+        print("잘못된 주문입니다")
 
-print(f"총 금액은 {(menus[0][1] * 2000) + (menus[1][1] * 2500) + (menus[2][1] * 2400)}원 입니다.")
+
+total_price = 0
+for j in range(len(menus)):
+    if menus[j][1] > 0:  # 각 메뉴들의 수량이 1 이상이면
+        total_price = total_price + (menus[j][1]* prices[j])  # 가격 리스트에서 가격 추출해서 합산
+
+print(f"총 금액은 {total_price}원 입니다.")
+#print(f"총 금액은 {(menus[0][1] * 2000) + (menus[1][1] * 2500) + (menus[2][1] * 2400)}원 입니다.")
